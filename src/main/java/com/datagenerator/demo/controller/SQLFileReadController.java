3c3468386fd2c4ad770a9c6799063575a9929d7e
arajak <arajak@OSIT-L-0417.osius.com>
2018-01-23 12:08:45 +0530

    added controller and service

--- src/main/java/com/datagenerator/demo/controller/SQLFileReadController.java
+++ src/main/java/com/datagenerator/demo/controller/SQLFileReadController.java
@@ -1,10 +1,25 @@
 package com.datagenerator.demo.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.datagenerator.demo.service.SQLFileReadService;
+
+@RestController
+@RequestMapping("/uploadfile")
 public class SQLFileReadController extends BaseController{
 	
+	@Autowired
+	SQLFileReadService sqlFileReadService;
 	
+	@PostMapping
+	public ResponseEntity<String> createPosition() throws Exception {
+		sqlFileReadService.readSQLfile();
+		return new ResponseEntity<>("uploaded successfully", HttpStatus.OK);
+	}
 
 }
--- src/main/java/com/datagenerator/demo/serviceImpl/SQLFileReadServiceImpl.java
+++ src/main/java/com/datagenerator/demo/serviceImpl/SQLFileReadServiceImpl.java
@@ -1,7 +1,10 @@
 package com.datagenerator.demo.serviceImpl;
 
+import org.springframework.stereotype.Service;
+
 import com.datagenerator.demo.service.SQLFileReadService;
 
+@Service
 public class SQLFileReadServiceImpl implements SQLFileReadService{
 
 	@Override
