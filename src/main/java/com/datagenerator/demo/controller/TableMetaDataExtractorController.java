2bbc80eeffcf094d6e6d50c1bbbf4fcb5412577a
dthorat <dthorat@OSIT-L-0531.osius.com>
2018-01-25 15:39:51 +0530

    [Datta]-"Committing changes for adding xmi parsing"

--- src/main/java/com/datagenerator/demo/controller/TableMetaDataExtractorController.java
+++ src/main/java/com/datagenerator/demo/controller/TableMetaDataExtractorController.java
@@ -2,7 +2,6 @@ package com.datagenerator.demo.controller;
 
 import java.io.FileNotFoundException;
 import java.util.LinkedHashMap;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.datagenerator.demo.utils.TableStructureExtractor;
+import com.datagenerator.demo.utils.XmiTableStructureExtractor;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,9 +23,13 @@ public class TableMetaDataExtractorController {
 	@Autowired
 	private TableStructureExtractor tableStructureExtractor;
 	
+	@Autowired
+	private XmiTableStructureExtractor XmiTableStructureExtractor;
+	
 	@GetMapping
 	public ResponseEntity<LinkedHashMap<String, LinkedHashMap<String,String>>> extractTableMetaData() throws FileNotFoundException {
 		LinkedHashMap<String, LinkedHashMap<String,String>> tableMap = tableStructureExtractor.searchforTableName();
+		XmiTableStructureExtractor.searchforTableName();
 		return new ResponseEntity<LinkedHashMap<String, LinkedHashMap<String,String>>>(tableMap, HttpStatus.OK);
 	}
 }
