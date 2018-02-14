2f5eb9d675957576ec24fcb93a947acf18088fc5
admin <nagendra_chekkapalli@gap.com>
2018-02-14 11:28:40 +0530

    das-001[nagendra] added logic for downloading excel file

--- src/main/java/com/datagenerator/demo/controller/UploadController.java
+++ src/main/java/com/datagenerator/demo/controller/UploadController.java
@@ -1,13 +1,13 @@
 package com.datagenerator.demo.controller;
 
-
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.LinkedHashMap;
 import java.util.List;
-
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamSource;
@@ -19,47 +19,49 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.datagenerator.demo.service.SQLFileReadService;
 import com.datagenerator.demo.serviceImpl.DataGenerationService;
-import com.datagenerator.demo.serviceImpl.UploadService;
 import com.datagenerator.demo.utils.CustomTokenConverter;
-
 
 @RestController
 public class UploadController {
-	
-	@Autowired
-	private	UploadService uploadService;
-	
+
+
 	@Autowired
 	SQLFileReadService sqlFileReadService;
-	
+
 	@Autowired
 	DataGenerationService dataGenerationService;
-	
+
 	@Autowired
 	private CustomTokenConverter customTokenConverter;
-	
+
+	@Autowired
+	private HttpServletResponse response;
+
 	@PostMapping("/upload")
-	public ResponseEntity<List<LinkedHashMap<String, LinkedHashMap<String,String>>>> uploadProfile(@RequestParam(name = "file", required = true) MultipartFile multipartFile,@RequestParam(name = "domainType", required = true)String domainType) throws Exception {
-		/*if (null == multipartFile) {
-			return new ResponseEntity<String>("Please select file for upload.!", HttpStatus.BAD_REQUEST);
-		}*/
-	//	uploadService.uploadFile(multipartFile);
-		List<LinkedHashMap<String, LinkedHashMap<String,String>>> list = sqlFileReadService.readSQLfile(multipartFile,domainType);
-		customTokenConverter.setAdditionalInfo("mappedTables",list);
-		return new ResponseEntity<List<LinkedHashMap<String, LinkedHashMap<String,String>>>>(list,HttpStatus.OK);
+	public ResponseEntity<List<LinkedHashMap<String, LinkedHashMap<String, String>>>> uploadProfile(
+			@RequestParam(name = "file", required = true) MultipartFile multipartFile,
+			@RequestParam(name = "domainType", required = true) String domainType) throws Exception {
+		/*
+		 * if (null == multipartFile) { return new
+		 * ResponseEntity<String>("Please select file for upload.!",
+		 * HttpStatus.BAD_REQUEST); }
+		 */
+		// uploadService.uploadFile(multipartFile);
+		List<LinkedHashMap<String, LinkedHashMap<String, String>>> list = sqlFileReadService.readSQLfile(multipartFile,
+				domainType);
+		customTokenConverter.setAdditionalInfo("mappedTables", list);
+		return new ResponseEntity<List<LinkedHashMap<String, LinkedHashMap<String, String>>>>(list, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/download")
 	public ResponseEntity<?> downloadFile() {
 		ClassLoader classLoader = getClass().getClassLoader();
 		File file = new File(classLoader.getResource("Dump20180122.sql").getFile());
-		InputStreamSource resource=null;
+		InputStreamSource resource = null;
 		try {
 			resource = new org.springframework.core.io.InputStreamResource(new java.io.FileInputStream(file));
 		} catch (FileNotFoundException e) {
@@ -68,14 +70,22 @@ public class UploadController {
 		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + file.getName())
 				.contentType(MediaType.APPLICATION_OCTET_STREAM).contentLength(file.length()).body(resource);
 	}
-	
+
 	@GetMapping("/downloadExcel")
-	public @ResponseBody ResponseEntity<?> downloadExcelFile() throws IOException {
+	public ResponseEntity<?> downloadExcelFile() throws Exception {
 		dataGenerationService.generateData();
 		Resource resource = new ClassPathResource("output/ExcelSheet.xls");
-		return ResponseEntity.ok()
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + resource.getFilename())
-				.contentType(MediaType.APPLICATION_XHTML_XML).contentLength(resource.getFile().length()).body(resource);
+		File file = resource.getFile();
+		BufferedInputStream isr = new BufferedInputStream(new FileInputStream(file));
+		if (file != null) {
+			response.setContentType("application/vnd.ms-excel");
+			response.setContentLength((int) file.length());
+			IOUtils.copyLarge(isr, response.getOutputStream());
+		} else {
+			isr.close();
+			throw new Exception("Excel file not found");
+		}
+		return new ResponseEntity<HttpServletResponse>(response, HttpStatus.OK);
 	}
-	
+
 }
--- src/main/java/com/datagenerator/demo/serviceImpl/DataGenerationService.java
+++ src/main/java/com/datagenerator/demo/serviceImpl/DataGenerationService.java
@@ -1,71 +1,33 @@
 package com.datagenerator.demo.serviceImpl;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CountDownLatch;
-
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-
 import com.datagenerator.demo.domain.GenerateDataObject;
+import com.datagenerator.demo.utils.CustomTokenConverter;
 import com.datagenerator.demo.utils.Worker;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class DataGenerationService {
-	
-	private static final String FKURI = "http://localhost:9080/datagenerator/getFKRelation";
 
-	private static final String URI = "http://localhost:9080/datagenerator/upload?domainType=SupplyChain";
-	private static RestTemplate restTemplate = new RestTemplate();
-	private static HttpHeaders headers = new HttpHeaders();
-	private static ObjectMapper mapper = new ObjectMapper();
 	public static List<LinkedHashMap<String, LinkedHashMap<String, String>>> tableMap = null;
 	private static List<GenerateDataObject> tableList = null;
 
-	
-	public void generateData () {
-		MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
-		try {
-			bodyMap.add("file", getUserFileResource());
-			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-			headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-			HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(bodyMap, headers);
-			ResponseEntity<String> json = restTemplate.exchange(URI, HttpMethod.POST, requestEntity, String.class);
-			System.out.println(json.getBody());
-			tableMap = mapper.readValue(json.getBody(),
-					new TypeReference<List<LinkedHashMap<String, LinkedHashMap<String, String>>>>() {
-					});
-			System.out.println("tableMap ::::" + tableMap);
-		
-		
-		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		ResponseEntity<String> jsonList = restTemplate.exchange(FKURI, HttpMethod.GET, entity, String.class);
-		 tableList = mapper.readValue(jsonList.getBody(),
-				new TypeReference<List<GenerateDataObject>>() {
-				});
-		System.out.println("size "+tableList.size());
-		System.out.println(tableList);
+	@Autowired
+	private CustomTokenConverter customTokenConverter;
+
+	public void generateData() {
+		tableMap = (List<LinkedHashMap<String, LinkedHashMap<String, String>>>) customTokenConverter
+				.getAdditionalInfo("orderedFKList");
+		tableList = (List<GenerateDataObject>) customTokenConverter.getAdditionalInfo("");
+
 		threadService(tableList);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+
 	}
-	
+
 	public static void threadService(List<GenerateDataObject> tableList) {
 
 		CountDownLatch countDownLatch = new CountDownLatch(tableList.size());
@@ -78,17 +40,11 @@ public class DataGenerationService {
 			workers.forEach(Thread::start);
 			countDownLatch.await();
 			System.out.println("Count down latch released");
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 
 	}
 
 	
-
-	public static Resource getUserFileResource() throws IOException {
-
-		return new ClassPathResource("mysqlsampledatabase.sql");
-	}
-
 }
