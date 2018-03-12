5269c67bae1d93f943f20d3140dec0e3e7b1c8e2
Suhail Sawant <ssawant@osius.com>
2018-03-12 16:28:48 +0530

    [Suhail] : added characterset while converting bytes to string

--- src/main/java/com/datagenerator/demo/datagen/ByteDataGenerator.java
+++ src/main/java/com/datagenerator/demo/datagen/ByteDataGenerator.java
@@ -4,6 +4,8 @@ import static com.datagenerator.demo.utils.DataGenUtil.singleQuote;
 
 import com.datagenerator.demo.datagen.intf.IDataGenerator;
 import com.datagenerator.demo.domain.Field;
+
+import java.io.UnsupportedEncodingException;
 import java.text.ParseException;
 import java.util.Random;
 
@@ -13,6 +15,12 @@ public class ByteDataGenerator implements IDataGenerator {
   public String generateData(Field field) throws ParseException {
     byte[] b = new byte[20];
     new Random().nextBytes(b);
-    return singleQuote((new String(b)));
+    try {
+		return singleQuote((new String(b, "UTF-16")));
+	} catch (UnsupportedEncodingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    return null;
   }
 }
